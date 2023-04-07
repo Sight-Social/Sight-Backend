7eb32f18b4239bd7f4e15d569434d52d1cd86e71
Sammy El-Sherif <elsherifsammy@gmail.com>
2023-04-07 09:30:52 -0600

    aws image upload functional

--- server/src/api/components/user/focalpointRoutes.js
+++ server/src/api/components/user/focalpointRoutes.js
@@ -4,6 +4,8 @@ const User = require('./model.js');
 const { default: mongoose } = require('mongoose');
 const jwt = require('jsonwebtoken');
 const authSight = require('../auth/authSight');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const multer = require('multer');
 
 /* ADD A FOCAL POINT */
 router.post('/', authSight, async (req, res) => {
@@ -23,11 +25,11 @@ router.post('/', authSight, async (req, res) => {
 
     user.focalpoints.push(newFocalPoint);
     await user.save();
-    console.log('Successfully added new focal point:', newFocalPoint)
+    console.log('Successfully added new focal point:', newFocalPoint);
 
     /* Grab most recently added focal point from db and return it */
-    const dbNewFocalPoint = user.focalpoints[user.focalpoints.length-1]
-    
+    const dbNewFocalPoint = user.focalpoints[user.focalpoints.length - 1];
+
     return res.status(201).json(dbNewFocalPoint);
   } catch (err) {
     console.error(err);
@@ -44,11 +46,13 @@ router.delete('/', authSight, async (req, res) => {
 
   try {
     const user = req.user;
-    console.log(req.body.focalpoint)
+    console.log(req.body.focalpoint);
     /* Remove the focal point from their focalpoints array */
     user.focalpoints.pull(req.body.focalpoint);
     await user.save();
-    console.log('Successfully removed focal point, sending back 201 & the deleted focal point..');
+    console.log(
+      'Successfully removed focal point, sending back 201 & the deleted focal point..'
+    );
     res.status(201).json(req.body.focalpoint);
   } catch (err) {
     console.error(err);
@@ -76,7 +80,9 @@ router.patch('/', authSight, async (req, res) => {
     user.focalpoints[focalpointIndex].title = editedName;
     user.focalpoints[focalpointIndex].description = editedDescription;
     await user.save();
-    console.log('Successfully updated focal point, sending back 200 & the updated user..')
+    console.log(
+      'Successfully updated focal point, sending back 200 & the updated user..'
+    );
     return res.status(200).send(user);
   } catch (err) {
     console.error(err);
@@ -84,22 +90,51 @@ router.patch('/', authSight, async (req, res) => {
   }
 });
 
+const upload = multer();
 /* EDIT A FOCAL POINT -> /:username/focalpoints */
-router.patch('/image', authSight, async (req, res) => {
+router.patch('/image', authSight, upload.single('file'), async (req, res) => {
   console.log('----------------------------------------');
   console.log('[PATCH] Got a request at /user/:username/focalpoints/image');
-
+  const S3_BUCKET = 'sight-image-bucket-323';
+  const REGION = 'us-east-1';
   const user = req.user;
-  const { imageUrl, focalpointIndex } = req.body;
-  console.log('imageUrl:', imageUrl);
-  console.log('focalpointIndex:', focalpointIndex);
+
+  /* FORM DATA */
+  const file = req.file; // The uploaded file
+  const key = req.body.key; // The "key" field
+  const focalpointIndex = req.body.focalpointIndex; // The "focalpointIndex" field
+  const focalpointId = req.body.focalpointId; // The "focalpointId" field
+
+  console.log('body:', req.body);
+  console.log('file:', req.file);
+
+  const creds = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  };
+
+  const s3Client = new S3Client({
+    region: REGION,
+    credentials: creds,
+  });
+
+  const command = new PutObjectCommand({
+    Bucket: S3_BUCKET,
+    Key: key,
+    Body: file.buffer,
+  });
+
+  const response = await s3Client.send(command);
+  console.log('RESPONSE:', response);
+  const url = `https://${S3_BUCKET}.s3.amazonaws.com/${key}`;
+  console.log('Image URl:', url);
+
   try {
     /* UPDATE the focal point's image url */
-    user.focalpoints[focalpointIndex].imageUrl = imageUrl;
+    user.focalpoints[focalpointIndex].imageUrl = url;
     await user.save();
-    /* console.log('updatedUser:', user); */
 
-    return res.status(201).send(imageUrl);
+    return res.status(201).send(url);
   } catch (err) {
     console.error(err);
     res.status(500).send({ message: 'Internal Server Error' });
